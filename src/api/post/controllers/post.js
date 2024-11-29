1cbd739c3b2cb137323406dc9199d91d427b5310
rafon <rafaelcarneiro16012003@gmail.com>
2024-11-29 09:30:41 -0300

    teste

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,8 +1,16 @@
 'use strict';
 
-/**
- * post controller
- */
+module.exports = () => {
+    // Add authenticated user to filter.
+    return async (ctx, next) => {
+      ctx.query.filters = {
+        ...ctx.query.filters,
+        users: [ctx.state.user.id]
+      };
+  
+      await next();
+    };
+  };
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
