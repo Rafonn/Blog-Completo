fa18284b76f649a87228eaf480998c666ffd2202
rafon <rafaelcarneiro16012003@gmail.com>
2024-12-05 09:22:52 -0300

    adicionado o dominio da netlify

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://polar-depths-60553-3a60956939d7.herokuapp.com/ap
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'Rafael Carneiro';
-export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1:3000';
+export const SITE_URL = process.env.SITE_URL || 'https://gregarious-mochi-c84896.netlify.app/';
