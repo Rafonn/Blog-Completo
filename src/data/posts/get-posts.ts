9067dd1a3da0b7858fa8dbff4105c1427e3a6608
rafon <rafaelcarneiro16012003@gmail.com>
2024-12-09 09:06:34 -0300

    fiz filters

--- src/data/posts/get-posts.ts
+++ src/data/posts/get-posts.ts
@@ -4,7 +4,7 @@ import { fetchJson } from "@/utils/fetch-json";
 import { MarkDownToHtml } from "@/utils/markdown-to-html";
 
 export const getPosts = async (slug: string | string[]): Promise<PostData[]> => {
-    const url = `${POSTS_URL}?filters[slug][$eq=${slug}&populate=%2A`;
+    const url = `${POSTS_URL}?populate=%2A&filters[slug]=${slug}`;
     const posts = await fetchJson<PostData[]>(url);
     const content = await MarkDownToHtml(posts[0].content);
     const finalContent = {...posts[0], content}
